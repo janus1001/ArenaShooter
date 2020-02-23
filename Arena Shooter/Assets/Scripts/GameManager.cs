b3317c8196dc42d8f07ec13ebc5d0d3d19ed6fbb
Jan Cenzartowicz <janusz.cimortowicz@gmail.com>
2020-02-23 03:59:12 +0000

    Game manager and HUD manager implemented
    None are working yet. Possibly both will be restructured from ground up.

---
+++ Arena Shooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameManager : NetworkBehaviour
+{
+    public static GameManager current;
+
+    [SyncVar(hook = "UpdateForestTeam")]
+    private TeamState forestTeamState;
+    [SyncVar(hook = "UpdateDesertTeam")]
+    private TeamState desertTeamState;
+    [SyncVar(hook = "UpdateIceTeam")]
+    private TeamState iceTeamState;
+    
+    // UpdateGameState is called when new game data changes
+    void UpdateForestTeam(TeamState oldValue, TeamState newValue)
+    {
+        
+    }
+    void UpdateDesertTeam(TeamState oldValue, TeamState newValue)
+    {
+        
+    }
+    void UpdateIceTeam(TeamState oldValue, TeamState newValue)
+    {
+        
+    }
+
+    private void Start()
+    {
+        if(current)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        current = this;
+    }
+}
+
+public struct TeamState
+{
+    private int baseHealth;
+    private int playerOneHealth;
+    private int playerTwoHealth;
+}
---
+++ Arena Shooter/Assets/Scripts/HUDManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public static HUDManager current;
+
+    public RectTransform offsetHUD;
+
+    public Image forestTeamHealth;
+    public List<HUDPlayer> forestTeamPlayers;
+
+    public Image desertTeamHealth;
+    public List<HUDPlayer> desertTeamPlayers;
+
+    public Image iceTeamHealth;
+    public List<HUDPlayer> iceTeamPlayers;
+
+    void Start()
+    {
+        if(current)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        current = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float value = Time.time % 1;
+    }
+}
+
+[Serializable]
+public class HUDPlayer
+{
+    [SerializeField]
+    public Image avatar;
+    [SerializeField]
+    public Image health;
+    [SerializeField]
+    public Image shield;
+    [SerializeField]
+    public Image crossout;
+}
