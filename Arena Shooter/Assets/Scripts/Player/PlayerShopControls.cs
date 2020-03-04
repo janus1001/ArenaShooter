52620ddaffdb03fa206d0fc23faf8758f463adee
Jan Cenzartowicz <janusz.cimortowicz@gmail.com>
2020-03-04 00:00:05 +0000

    Item shop work and backup
    Starting work on Room System and may need to redownload the project

---
+++ Arena Shooter/Assets/Scripts/Player/PlayerShopControls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerShopControls : NetworkBehaviour
+{
+    int raycastLayer;
+    private int maxDistance = 3;
+    GameObject lastHitShop = null;
+
+    void Start()
+    {
+        raycastLayer = LayerMask.GetMask("Shop Option");
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit raycastHit, maxDistance, raycastLayer))
+        {
+            GameObject shop = raycastHit.collider.gameObject;
+            if (shop != lastHitShop)
+            {
+                if (lastHitShop)
+                {
+                    lastHitShop.GetComponentInParent<Shop>().HideDetails();
+                }
+                lastHitShop = shop;
+                lastHitShop.GetComponentInParent<Shop>().ShowDetails();
+                maxDistance = 5;
+            }
+        }
+        else if (lastHitShop)
+        {
+            lastHitShop.GetComponentInParent<Shop>().HideDetails();
+            lastHitShop = null;
+            maxDistance = 3;
+        }
+    }
+
+    [Command]
+    void CmdTryBuy(NetworkIdentity shopIdentity)
+    {
+
+    }
+}
--- Arena Shooter/Assets/Scripts/Shop.cs
+++ Arena Shooter/Assets/Scripts/Shop.cs
@@ -1,19 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Shop : MonoBehaviour
+public class Shop : NetworkBehaviour
 {
     public GameObject itemPreview;
+    public Transform detailsPage;
+
     public Buyable soldItem;
+
+    float slideTarget = 0f;
+
+    private void Update()
+    {
+        SetDetailsPosition();
+    }
+
+    private void SetDetailsPosition()
+    {
+        Vector3 newLocalPosition = detailsPage.localPosition;
+        newLocalPosition.y = Mathf.Lerp(newLocalPosition.y, slideTarget, 5f * Time.deltaTime);
+        detailsPage.localPosition = newLocalPosition;
+    }
 
     public void ShowDetails()
     {
-
+        slideTarget = -10f;
     }
 
     public void HideDetails()
     {
-
+        slideTarget = 0f;
     }
 }
