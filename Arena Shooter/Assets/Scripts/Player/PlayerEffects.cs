b22599f655c27092744e9f8ff1fe7088e241753a
Krzysztof Nowacki <krzysow@gmail.com>
2020-05-03 20:47:58 +0200

    Animation first attempt

--- Arena Shooter/Assets/Scripts/Player/PlayerEffects.cs
+++ Arena Shooter/Assets/Scripts/Player/PlayerEffects.cs
@@ -5,15 +5,21 @@ using Mirror;
 
 public class PlayerEffects : NetworkBehaviour
 {
+    public Animator animator;
+    Vector3 positionLastFrame;
+    float walkingSpeed = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        positionLastFrame = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        walkingSpeed = Vector3.Distance(positionLastFrame, gameObject.transform.position);
+        animator.SetFloat("walkingSpeed", walkingSpeed);
+        positionLastFrame = gameObject.transform.position;
     }
 }
